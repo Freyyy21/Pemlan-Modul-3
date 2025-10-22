8673833c2034cde7f4b562dc6b70badf814b0d93
Freyyy21 <dgraymaiden@gmail.com>
2025-10-22 11:21:45 +0700

    first commit

---
+++ Codelab/Codelab1.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Codelab1 {
+
+    public static void main(String[] args) {
+        //Menghitung luas persegi panjang
+
+        double panjang;
+        double lebar;
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.print("Masukkan panjang: ");
+                panjang = input.nextDouble();
+                System.out.print("Masukkan lebar: ");
+                lebar = input.nextDouble();
+                double luas = panjang * lebar;
+                System.out.println("Luas dari persegi panjang " + panjang + " x " + lebar + " adalah " + luas);
+                break;
+
+            } catch (InputMismatchException e) {
+                System.out.println("Input tidak valid!");
+                input.nextLine();
+            }
+
+        }
+
+
+        input.close();
+    }
+}
---
+++ Codelab/Codelab2.java
@@ -0,0 +1,2 @@
+public class Codelab2 {
+}
