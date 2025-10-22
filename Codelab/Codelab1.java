e26598211eb880d3cb74fca8d0fc66ff6bc319bc
Freyyy21 <dgraymaiden@gmail.com>
2025-10-22 11:25:31 +0700

    third test

--- Codelab/Codelab1.java
+++ Codelab/Codelab1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Codelab1 {
 
     public static void main(String[] args) {
-        //Program untuk Menghitung luas persegi panjang
+        //Program Menghitung luas persegi panjang
 
         double panjang;
         double lebar;
